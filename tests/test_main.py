e7aca659f053b699154e70f7c3716e8e9235544b
Josh Greenslade <joshua.greenslade@onyxinsight.com>
2021-12-13 21:06:23 +0000

    CD pipeline

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,2 +1,9 @@
-def test_pass():
-    return True
+from .fixtures import client
+
+def test_root(client):
+    res = client.get('/')
+    message = res.json()['message']
+
+    assert message
+    assert res.status_code == 200
+    
